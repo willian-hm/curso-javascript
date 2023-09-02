80144a78e2f8d48b3e3ab7a652a9226720a8382b
willianmoreira45 <willianmoreira0510@gmail.com>
2023-09-02 18:46:38 -0300

    Estrutura de rep

--- script.js
+++ script.js
@@ -1,18 +1,84 @@
 
-var y;
+// var y;
 
-y = 37;
-document.write(y);
-document.write("<br>")
+// y = 37;
+// document.write(y);
+// document.write("<br>")
 
-y = ("Will");
-document.write(y);
-document.write("<br>")
+// y = ("Willian");
+// document.write(y);
+// document.write("<br>")
 
-y = true;
-document.write(y);
-document.write("<br>")
+// y = true;
+// document.write(y);
+// document.write("<br>")
 
-y = null;
-document.write(y);
-document.write("<br>")
+// y = null;
+// document.write(y);
+// document.write("<br>")
+
+// var expressao;
+
+// expressao = 30 + 20 - 10*5
+
+// document.write ("30 + 20 - 10 * 5 = ")
+// document.write (expressao)
+
+// if (condicao) {
+//     declaracao_1;
+// } else if (condicao_2) {
+//     declaracao_2;
+// } else if (condicao_n) {
+//     declaracao_n;
+// } else (condicao_final) {
+//     declaracao_final;
+// }
+
+// var idade;
+// idade = prompt ("digite sua idade: ")
+
+// if (idade >= 18) {
+//     var maiorIdade = true;
+//    document.write("Maior de idade")
+// } else {
+//     var maiorIdade = false;
+//     document.write ("Menor de idade")
+// }
+
+// cargo = prompt("digite seu cargo: ")
+// salario = 2000
+// 
+// switch (cargo) {
+//     case "gerente":
+//         salario *= 1.15; // salario = salario * 1.15
+//         break;
+//     case "supervisor":
+//         salario *= 1.10; // salario = salario * 1.10
+//         break;  
+//     default:
+//         salario *= 1.05; // salario = salario * 1.05
+// }
+
+// console.log("Salario do "+ cargo +": " + salario)
+
+// n = 0;
+// x = 0;
+// while (n < 4) {
+//     n++;
+//    x += n; // x = x+n;
+//     console.log(x);
+// }
+
+// i = 0
+
+// do {
+//     i = i + 1;
+//     console.log(i)
+// } while (i < 5)
+
+var cont;
+
+for(cont = 0; cont < 10; cont++)
+{
+    console.log("Willian")
+}
