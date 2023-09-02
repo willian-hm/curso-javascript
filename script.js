7fbde8428a1431c248ff497a9917d795e2afcb3b
willianmoreira45 <willianmoreira0510@gmail.com>
2023-09-02 17:58:29 -0300

    var

---
+++ script.js
@@ -0,0 +1,18 @@
+
+var y;
+
+y = 37;
+document.write(y);
+document.write("<br>")
+
+y = ("Will");
+document.write(y);
+document.write("<br>")
+
+y = true;
+document.write(y);
+document.write("<br>")
+
+y = null;
+document.write(y);
+document.write("<br>")
